a0566ff7155dfb1c0cf88261f9b1a59b8f6a5577
unknown <dierongamer04@gmail.com>
2022-03-28 12:19:48 +0800

    Finished board and piece visuals

--- checkers/piece.py
+++ checkers/piece.py
@@ -1,5 +1,5 @@
 import pygame
-from .constants import SQUARE_SIZE, WHITE, GRAY, BLACK
+from .constants import SQUARE_SIZE, WHITE, GRAY, BLACK, CROWN
 
 class Piece:
     PADDING = 10
@@ -19,18 +19,27 @@ class Piece:
         
         self.x = 0
         self.y = 0
+        self.calculate_pos()
 
-        def calculate_pos(self):
-            self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
-            self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
+    def calculate_pos(self):
+        self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
+        self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
 
-        def make_king(self):
-            self.king = True
+    def make_king(self):
+        self.king = True
 
-        def draw(self, win):
-            radius = SQUARE_SIZE // 2 - self.PADDING
-            pygame.draw.circle(win, GRAY, (self.x, self.y), radius + self.OUTLINE)
-            pygame.draw.circle(win, self.color, (self.x, self.y), radius)
+    def draw(self, win):
+        radius = SQUARE_SIZE // 2 - self.PADDING
+        pygame.draw.circle(win, GRAY, (self.x, self.y), radius + self.OUTLINE)
+        pygame.draw.circle(win, self.color, (self.x, self.y), radius)
 
-        def __repr__(self):
-            return str(self.color)
+        if self.king:
+            win.blit(CROWN, (self.x - CROWN.get_width() // 2, self.y - CROWN.get_height() // 2))
+
+    def move(self, row, col):
+        self.row = row
+        self.col = col
+        self.calculate_pos()
+
+    def __repr__(self):
+        return str(self.color)
