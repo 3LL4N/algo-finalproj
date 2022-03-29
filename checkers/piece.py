4566adb5aed2518914b2afa0a041e30f3ac381b9
unknown <dierongamer04@gmail.com>
2022-03-29 13:06:27 +0800

    Added functions to determine valid moves

--- checkers/piece.py
+++ checkers/piece.py
@@ -10,13 +10,6 @@ class Piece:
         self.col = col
         self.color = color
         self.king = False
-
-        # Direction of which is piece is supposed to be going
-        if self.color == WHITE:
-            self.direction = -1
-        else:
-            self.direction = 1
-        
         self.x = 0
         self.y = 0
         self.calculate_pos()
